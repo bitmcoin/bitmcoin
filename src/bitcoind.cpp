48e478c9405fe659f5669d2da9c5771bcbf7ad12
BITM <bitmcoincore@gmail.com>
2026-08-28 22:35:17 +0800

    Update bitcoind.cpp

--- src/bitcoind.cpp
+++ src/bitcoind.cpp
@@ -83,7 +83,7 @@ bool AppInit(int argc, char* argv[])
         else
         {
             strUsage += "\n" + _("Usage:") + "\n" +
-                  "  bitcoind [options]                     " + _("Start Bitmcoin Core Daemon") + "\n";
+                  "  bitmcoind [options]                     " + _("Start Bitmcoin Core Daemon") + "\n";
 
             strUsage += "\n" + HelpMessage(HMM_BITCOIND);
         }
@@ -117,12 +117,12 @@ bool AppInit(int argc, char* argv[])
         // Command-line RPC
         bool fCommandLine = false;
         for (int i = 1; i < argc; i++)
-            if (!IsSwitchChar(argv[i][0]) && !boost::algorithm::istarts_with(argv[i], "bitcoin:"))
+            if (!IsSwitchChar(argv[i][0]) && !boost::algorithm::istarts_with(argv[i], "bitmcoin:"))
                 fCommandLine = true;
 
         if (fCommandLine)
         {
-            fprintf(stderr, "Error: There is no RPC client functionality in bitcoind anymore. Use the bitcoin-cli utility instead.\n");
+            fprintf(stderr, "Error: There is no RPC client functionality in bitmcoind anymore. Use the bitmcoin-cli utility instead.\n");
             exit(1);
         }
 #ifndef WIN32
